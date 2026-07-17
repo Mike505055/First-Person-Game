6def39eb192550e760634c8d6f1808a047c54a25
Mike505055 <michaelmagistro@yahoo.com>
2026-07-17 14:30:20 -0400

    Added staff and camera movement

--- Role Playing/Assets/Scripts/Playermovement.cs
+++ Role Playing/Assets/Scripts/Playermovement.cs
@@ -6,8 +6,10 @@ public class Playermovement : MonoBehaviour
     public float speed = 5f;
     public float jumpHeight = 1.5f;
     public float gravity = -20f;
+    public float mouseSensitivity = 200f;
     private CharacterController controller;
     private float verticalVelocity;
+
 
 
 
@@ -18,21 +20,29 @@ public class Playermovement : MonoBehaviour
 
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // camera controls
+        float mouseX = Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
+        Camera.main.transform.Rotate(Vector3.right * mouseY);
+        // Takes in the input and moves
         float xInput = Input.GetAxisRaw("Horizontal");
         float zInput = Input.GetAxisRaw("Vertical");
         Vector3 moveDirection = transform.right * xInput + transform.forward * zInput;
         moveDirection = Vector3.ClampMagnitude(moveDirection, 1f);
-
+        
 
 
 
         
-        
+        // Controls gravity and the player jump
         if (controller.isGrounded && verticalVelocity<0f)
         {
             verticalVelocity = -2f;
