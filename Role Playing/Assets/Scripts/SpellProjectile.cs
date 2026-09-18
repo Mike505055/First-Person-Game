64a6e96306e626ba5f56f08e438b0329eed2c1a5
Mike505055 <michaelmagistro@yahoo.com>
2026-09-18 13:37:39 -0400

    Added fire explosion

--- Role Playing/Assets/Scripts/SpellProjectile.cs
+++ Role Playing/Assets/Scripts/SpellProjectile.cs
@@ -4,8 +4,9 @@ public class SpellProjectile : MonoBehaviour
 {
     public Rigidbody rb;
     public float flyingSpeed;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject explosionParticle;
+    public float PushingForce;
+    // Start is called once before theng first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
@@ -14,6 +15,20 @@ public class SpellProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         rb.MovePosition(transform.position + transform.forward * flyingSpeed * Time.deltaTime);
+
+
+
     }
+    void OnTriggerEnter(Collider other)
+    {
+        Vector3 direction = other.transform.position - transform.position;
+        other.GetComponent<Rigidbody>().AddForce(direction*PushingForce, ForceMode.Impulse);
+        Instantiate(explosionParticle,transform.position,Quaternion.identity); 
+        Destroy(gameObject);
+    }
+
+
+
 }
